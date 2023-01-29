e1522f53e436c7e27b6dda81ed674a6ff0d0e4cc
Ahters88 <110418015+Ahters88@users.noreply.github.com>
2023-01-29 14:12:40 +0100

    konfiguracja zadanie 3

---
+++ SeleniumCourse/src/test/java/Konfiguracja/Main03.java
@@ -0,0 +1,31 @@
+package Konfiguracja;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main03 {
+    public static void main(String[] args) throws  InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        WebElement acceptCookiesButton = driver.findElement(By.id("L2AGLb"));
+        acceptCookiesButton.click();
+        Thread.sleep(5000);
+        driver.get("https://coderslab.pl/pl");
+        driver.navigate().back();
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        driver.quit();
+
+
+
+    }
+}
